ad302a7fa40d9464cc9a26750ce65b6af28404e2
Alexandre Muzulão <alexandre.muzulao@tecnospeed.com.br>
2018-06-12 15:10:01 -0300

    Incluindo Alert com avaliação

--- www/js/index.js
+++ www/js/index.js
@@ -5,3 +5,33 @@ function goDetail() {
         });
     })
 }
+
+function showMyCustomizedAlert(){
+    alert({
+    title:'My alert',
+    message:'This is my customized alert!',
+    buttons:[
+        {
+        label: 'OK',
+        onclick: function(){
+            //You code when user click in OK button.
+            closeAlert();
+        }
+        },
+        {
+        label:'Cancel',
+        onclick: function(){
+            closeAlert();
+        }
+        }
+    ]
+    });
+}
+
+function avaliar(id) {
+    var box = 'Quem é seu(sua) amigo(a) no rolê? <br/><br/>';
+    box += '<button class="text-small margin-bottom full green radius padding">Chapa no Rolê</button>';
+    box += '<button class="text-small margin-bottom full purple radius padding">Vai as vezes</button>';
+    box += '<button class="text-small margin-bottom full red full radius padding">Nem Chama</button>';
+    alert(box, 'Avaliação')
+}
