2d9f3e7f4af39dcaedc05bdc6562d63f60a09143
Alexandre-Muzulao <alexandre_muzulao@hotmail.com>
2018-06-13 00:30:07 -0300

    Mudando mensagnes de convite

--- www/js/index.js
+++ www/js/index.js
@@ -6,32 +6,45 @@ function goDetail() {
     })
 }
 
-function showMyCustomizedAlert(){
+function showMyCustomizedAlert(content, message){
     alert({
-    title:'My alert',
-    message:'This is my customized alert!',
+    title:message,
+    message:content,
+    class:'blue',
     buttons:[
         {
-        label: 'OK',
-        onclick: function(){
-            //You code when user click in OK button.
-            closeAlert();
-        }
+            label: 'OK',
+            class:'text-white',
+            onclick: function(){
+                //You code when user click in OK button.
+                closeAlert();
+            }
         },
         {
-        label:'Cancel',
-        onclick: function(){
-            closeAlert();
-        }
+            label:'Cancel',
+            class:'text-white',
+            onclick: function(){
+                closeAlert();
+            }
         }
     ]
     });
 }
 
-function avaliar(id) {
-    var box = 'Quem é seu(sua) amigo(a) no rolê? <br/><br/>';
+function avaliar(name){
+    var box = 'Quem é '+ name +' no rolê? <br/><br/>';
     box += '<button class="text-small margin-bottom full green radius padding">Chapa no Rolê</button>';
     box += '<button class="text-small margin-bottom full purple radius padding">Vai as vezes</button>';
     box += '<button class="text-small margin-bottom full red full radius padding">Nem Chama</button>';
-    alert(box, 'Avaliação')
+    showMyCustomizedAlert(box, 'Avaliar')
+}
+
+function convidar(nameUser, nameBar, nameBeer, valueDecimal, valueCentavos){
+    // var box = '<div class="blue align-center padding">';
+    var box = 'Enviar convite para ' + nameUser + ' ?'
+    box += '<p> Oii ' + nameUser + ' vamos lá no ' + nameBar + ' tomar uma ' + nameBeer + ' ????</p>';
+    box += '<p>Tá só R$ ' + valueDecimal + ',' + valueCentavos + ' !!!</p>';
+    box += '<p> Tá ai a Localização... BoraBeber!!!</p>';
+    // box += '</div>';
+    showMyCustomizedAlert(box, 'Convidar')
 }
